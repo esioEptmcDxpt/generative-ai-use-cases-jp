1986fdf4c4aa86826a3fe23d880611bca38d9b28
Kentaro Nagayama <k-nagayama@jreast.co.jp>
2025-07-25 00:32:44 +0900

    dev2 deploy - test phase

--- packages/cdk/parameter.ts
+++ packages/cdk/parameter.ts
@@ -20,8 +20,43 @@ const envs: Record<string, Partial<StackInput>> = {
   //   // Parameters for anonymous environment
   //   // If you want to override the default settings, add the following
   // },
-  dev: {
+  dev2: {
     // Parameters for development environment
+    modelRegion: 'us-west-2',
+    modelIds: [
+      'us.anthropic.claude-3-7-sonnet-20250219-v1:0',
+      'anthropic.claude-3-5-sonnet-20241022-v2:0',
+      'anthropic.claude-3-5-haiku-20241022-v1:0',
+      'us.amazon.nova-pro-v1:0',
+      'us.amazon.nova-lite-v1:0',
+      'us.amazon.nova-micro-v1:0',
+      'us.meta.llama3-3-70b-instruct-v1:0',
+      'meta.llama3-1-70b-instruct-v1:0',
+      'meta.llama3-1-8b-instruct-v1:0',
+      'cohere.command-r-plus-v1:0',
+      'cohere.command-r-v1:0',
+      'mistral.mistral-large-2407-v1:0',
+    ],
+    imageGenerationModelIds: [
+      'stability.stable-image-ultra-v1:1',
+      'stability.sd3-5-large-v1:0',
+      'stability.stable-image-core-v1:1',
+      'amazon.titan-image-generator-v2:0',
+      'amazon.titan-image-generator-v1',
+    ],
+    videoGenerationModelIds: ['amazon.nova-reel-v1:0', 'luma.ray-v2:0'],
+    ragEnabled: true,
+    kendraIndexArn:
+      'arn:aws:kendra:ap-northeast-1:326497581172:index/3ce313b7-4bfb-4257-8127-11db308dfdbe',
+    kendraDataSourceBucketName: 'jre-regulations',
+    ragKnowledgeBaseEnabled: true,
+    ragKnowledgeBaseId: 'PQSVG6HETU',
+    ragKnowledgeBaseStandbyReplicas: false,
+    ragKnowledgeBaseAdvancedParsing: false,
+    ragKnowledgeBaseAdvancedParsingModelId:
+      'anthropic.claude-3-sonnet-20240229-v1:0',
+    embeddingModelId: 'amazon.titan-embed-text-v2:0',
+    allowedSignUpEmailDomains: ['jreast.co.jp'],
   },
   staging: {
     // Parameters for staging environment
